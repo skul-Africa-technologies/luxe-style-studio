8004978115f9dfaadd92b2d868c6615d52a5e350
vig-gray <victorygray59@gmail.com>
2026-05-23 00:38:50 +0100

    up

--- backend/src/contact/dto/update-contact.dto.ts
+++ backend/src/contact/dto/update-contact.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateContactDto } from './create-contact.dto';
+import { ContactDto } from './create-contact.dto';
 
-export class UpdateContactDto extends PartialType(CreateContactDto) {}
+
+export class UpdateContactDto extends PartialType(ContactDto) {}
